58f5e33b9c9c62c26591380a67a569c7547eedef
senpang-coder <senpang@usc.edu>
2026-02-20 16:07:33 -0800

    data

---
+++ stimulation_data-2.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： localhost
+-- 生成日期： 2026-02-21 01:06:57
+-- 服务器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `oil_wells`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `stimulation_data`
+--
+
+CREATE TABLE `stimulation_data` (
+  `id` int(11) NOT NULL,
+  `well_id` int(11) DEFAULT NULL,
+  `treatment_type` varchar(255) DEFAULT NULL,
+  `total_proppant` double DEFAULT NULL,
+  `fluid_volume` double DEFAULT NULL,
+  `max_pressure` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 转存表中的数据 `stimulation_data`
+--
+
+INSERT INTO `stimulation_data` (`id`, `well_id`, `treatment_type`, `total_proppant`, `fluid_volume`, `max_pressure`) VALUES
+(53, 2, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(54, 4, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(55, 6, 'Frac / Fracture Treatment', NULL, NULL, 12500),
+(56, 8, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(57, 9, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(58, 10, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(59, 11, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(60, 12, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(61, 13, 'Frac / Fracture Treatment', NULL, NULL, 12500),
+(62, 14, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(63, 16, 'Frac / Fracture Treatment', NULL, NULL, 12500),
+(64, 17, NULL, NULL, NULL, 4490),
+(65, 21, 'Frac / Fracture Treatment', NULL, NULL, 12500),
+(66, 27, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(67, 28, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(68, 29, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(69, 31, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(70, 32, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(71, 36, 'Frac / Fracture Treatment', NULL, NULL, 9000),
+(72, 39, NULL, NULL, NULL, 500),
+(73, 41, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(74, 42, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(75, 43, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(76, 44, 'Frac / Fracture Treatment', NULL, NULL, NULL),
+(77, 45, 'Frac / Fracture Treatment', NULL, NULL, NULL);
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `stimulation_data`
+--
+ALTER TABLE `stimulation_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_stimulation_data_well` (`well_id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `stimulation_data`
+--
+ALTER TABLE `stimulation_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `stimulation_data`
+--
+ALTER TABLE `stimulation_data`
+  ADD CONSTRAINT `stimulation_data_ibfk_1` FOREIGN KEY (`well_id`) REFERENCES `wells` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
